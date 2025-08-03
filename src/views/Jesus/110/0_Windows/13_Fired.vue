1699d1ef742c6085c118a97b177d2be8b8154533
FDR-3 <fdr3@m4a.io>
2025-08-03 08:05:12 -0400

    Adding replacemented illegally fired video

--- src/views/Jesus/110/0_Windows/13_Fired.vue
+++ src/views/Jesus/110/0_Windows/13_Fired.vue
@@ -3,15 +3,28 @@
     <h1>How I Was Illegally Fired In Retaliation 12/12</h1>
     <div class="flexCenterRow">
       <PrevPageButton/>
-      <!--<media-player class="videoContainer" src="src/assets/video/Jesus Debate/110/12_VSDC/VSDC.mp4">
+      <!--<media-player class="videoContainer" src="https://mega.nz/file/8BATlSjL#G-A3GG4_Mlejp4Qgz416S1BhMzqAAylz26lUx0CCoWM">
         <media-provider><track src="" kind="subtitles" label="English" lang="en-US" default/></media-provider>
         <media-plyr-layout thumbnails=""></media-plyr-layout>
       </media-player>-->
       <iframe class="videoContainer"
-        src="https://www.youtube.com/embed/7sT6JFPbjPA?cc_load_policy=1&cc_lang_pref=en"
+        src="https://mega.nz/embed/sAhkTCYB#Hj-5uJzek8ghx1_G2XPIJ3wJyGh3v-7_TuBIdIWs9ZI"
         allowfullscreen
       ></iframe>
+      
+      <!--<iframe class="videoContainer"
+        src="https://www.youtube.com/embed/7sT6JFPbjPA?cc_load_policy=1&cc_lang_pref=en"
+        allowfullscreen
+      ></iframe>-->
       <NextPageButton/>
+    </div>
+    <div>
+      <p><a href="https://www.patriotsoftware.com/" target="_blank">Patriot Software</a> 
+        put a <a href="https://2yhveg6ijh.ufs.sh/f/ePibqLYvGazN22b7BKPGEneMSta34Q8BjfkgmswxvVZpTlDC" target="_blank">privacy claim</a> on my youtube video
+        <a href="https://www.youtube.com/watch?v=7sT6JFPbjPA" target="_blank">Here</a>
+        and had it taken down. I've contacted youtube about how this was a recording of them committing a crime
+        to have it put back up, but I've also created this new link in the mean time. Can you imagine a criminal complaining about you recording their illegal activities and wanting the video of their crimes down for privacy? XD Nah
+      </p>
     </div>
     <div class="sources">
       <h1>Sources</h1>
